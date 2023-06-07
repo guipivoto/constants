f2e19770a7dcc6f0a10f793b52782f4d22bf99cf
Guilherme Pivoto <guipivoto@gmail.com>
2023-06-07 00:41:15 -0300

    Migrate from Groovy to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import java.util.*
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+
+plugins {
+    id("com.android.application") version "8.0.1" apply false
+    id("com.android.library") version "8.0.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0" apply true
+}
+
+group = "com.anandroiddev"
+version = "0.0.2"
+
+var signingKeyId = ""
+var signingKey = ""
+var signingPassword = ""
+var ossrhUsername = ""
+var ossrhPassword = ""
+var sonatypeStagingProfileId = ""
+
+val propsFile = rootProject.file("keystore.properties")
+if (propsFile.exists()) {
+    val props = Properties().apply {
+        load(FileInputStream(propsFile))
+    }
+    signingKeyId = props.getProperty("signing.keyId")
+    signingKey = props.getProperty("signing.key")
+    signingPassword = props.getProperty("signing.password")
+    ossrhUsername = props.getProperty("ossrhUsername")
+    ossrhPassword = props.getProperty("ossrhPassword")
+    sonatypeStagingProfileId = props.getProperty("sonatypeStagingProfileId")
+} else {
+    throw FileNotFoundException("Key store file not found")
+}
+
+nexusPublishing {
+    repositories {
+        create("myNexus") {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(ossrhUsername)
+            password.set(ossrhPassword)
+        }
+    }
+}
---
+++ constants/build.gradle.kts
@@ -0,0 +1,122 @@
+@file:Suppress("UnstableApiUsage")
+
+import java.util.*
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("signing")
+}
+
+var signingKeyId = ""
+var signingKey = ""
+var signingPassword = ""
+var ossrhUsername = ""
+var ossrhPassword = ""
+var sonatypeStagingProfileId = ""
+
+val propsFile = rootProject.file("keystore.properties")
+if (propsFile.exists()) {
+    val props = Properties().apply {
+        load(FileInputStream(propsFile))
+    }
+    signingKeyId = props.getProperty("signing.keyId")
+    signingKey = props.getProperty("signing.key")
+    signingPassword = props.getProperty("signing.password")
+    ossrhUsername = props.getProperty("ossrhUsername")
+    ossrhPassword = props.getProperty("ossrhPassword")
+    sonatypeStagingProfileId = props.getProperty("sonatypeStagingProfileId")
+} else {
+    throw FileNotFoundException("Key store file not found")
+}
+
+android {
+    namespace  = "com.anandroiddev.constants"
+    compileSdk = 33
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+
+    defaultConfig.apply {
+        minSdk = 16
+        targetSdk = 33
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+tasks {
+    create<Jar>("androidSourcesJar") {
+        archiveClassifier.set("sources")
+        from(android.sourceSets["main"].java.srcDirs)
+        from(android.sourceSets["main"].kotlin)
+    }
+}
+
+artifacts {
+    archives(tasks["androidSourcesJar"])
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                pom {
+                    groupId = rootProject.group as String
+                    version = rootProject.version as String
+                    artifactId = "constants"
+
+                    name.set("An Android Dev Constants")
+                    description.set("Small library containing several constants frequently used on any project")
+                    url.set("https://github.com/guipivoto/constants")
+
+                    from(components.getByName("release"))
+
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("http://www.opensource.org/licenses/mit-license.php")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("guipivoto")
+                            name.set("Guilherme Pivoto")
+                            email.set("guipivoto@gmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:ssh://example.com/my-library.git")
+                        developerConnection.set("scm:git:ssh://example.com/my-library.git")
+                        url.set("https://github.com/guipivoto/constants")
+                    }
+                }
+            }
+        }
+   }
+
+    signing {
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+        sign(publishing.publications["mavenJava"])
+    }
+}
